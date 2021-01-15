8aa1a8ee710ee6dbd370597a6a821d0e8e27fa99
Jyri <54314515+jYKKEEE@users.noreply.github.com>
2021-01-15 22:49:08 +0200

    last working

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -1,17 +1,10 @@
 import styles from "./settings.module.scss";
 
-import Button from "../button/button";
-
 function Settings(props) {
   return (
     <div className={styles.settings}>
       <div>
-        {" "}
         <h2>Settings</h2>
-      </div>
-      <div className={styles.button}>
-        <Button text={"Change theme"} />
-        <Button text={"Exit WMS"} />
       </div>
     </div>
   );
