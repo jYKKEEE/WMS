94398a1ccf01a7a21b48805102f800266ecdd13b
Jyri <54314515+jYKKEEE@users.noreply.github.com>
2021-01-22 19:05:38 +0200

    tesdata added

---
+++ src/testdata.js
@@ -0,0 +1,414 @@
+const data = [
+  {
+    id: 1,
+    slots: [
+      {
+        barcode: 8512758022,
+        slot: 1,
+        level: 0,
+        products: [
+          {
+            id: 65464,
+            name: "Autonrengas",
+            barcode: 8512758022,
+            slot: 1,
+            level: 0,
+          },
+          {
+            id: 65469,
+            name: "Pakoputki",
+            barcode: 8512758022,
+            slot: 1,
+            level: 0,
+          },
+          {
+            id: 65471,
+            name: "Moottoriöljy",
+            barcode: 8512758022,
+            slot: 1,
+            level: 0,
+          },
+        ],
+      },
+      {
+        barcode: 8512758022,
+        slot: 1,
+        level: 1,
+        products: [],
+      },
+      {
+        barcode: 8512758022,
+        slot: 1,
+        level: 2,
+        products: [],
+      },
+      {
+        barcode: 8512758023,
+        slot: 2,
+        level: 0,
+        products: [
+          {
+            id: 65466,
+            name: "Moottori",
+            barcode: 8512758023,
+            slot: 2,
+            level: 0,
+          },
+        ],
+      },
+      {
+        barcode: 8512758024,
+        slot: 2,
+        level: 1,
+        products: [
+          {
+            id: 65399,
+            name: "Tuulilasi",
+            barcode: 8512758024,
+            slot: 2,
+            level: 1,
+          },
+          {
+            id: 65473,
+            name: "Tunkki",
+            barcode: 8512758024,
+            slot: 2,
+            level: 1,
+          },
+          {
+            id: 65499,
+            name: "Jarrupalat",
+            barcode: 8512758024,
+            slot: 2,
+            level: 1,
+          },
+        ],
+      },
+      {
+        barcode: 8512758098,
+        slot: 2,
+        level: 2,
+        products: [
+          {
+            id: 12236,
+            name: "Rengasavain",
+            barcode: 8512758098,
+            slot: 2,
+            level: 2,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    slots: [
+      {
+        barcode: 1630990742,
+        slot: 1,
+        level: 0,
+        products: [
+          {
+            id: 87659,
+            name: "Kissan hiekkaa",
+            barcode: 1630990742,
+            slot: 1,
+            level: 0,
+          },
+          {
+            id: 34523,
+            name: "Kiviä",
+            barcode: 1630990742,
+            slot: 1,
+            level: 0,
+          },
+          {
+            id: 34589,
+            name: "Jakoavain",
+            barcode: 1630990742,
+            slot: 1,
+            level: 0,
+          },
+          {
+            id: 87657,
+            name: "Vasara",
+            barcode: 1630990742,
+            slot: 1,
+            level: 0,
+          },
+        ],
+      },
+      {
+        barcode: 1630999999,
+        slot: 1,
+        level: 1,
+        products: [],
+      },
+      {
+        barcode: 3322225525,
+        slot: 2,
+        level: 0,
+        products: [],
+      },
+      {
+        barcode: 3377625525,
+        slot: 2,
+        level: 1,
+        products: [],
+      },
+      {
+        barcode: 3377625525,
+        slot: 2,
+        level: 2,
+        products: [],
+      },
+      {
+        barcode: 3377625525,
+        slot: 2,
+        level: 3,
+        products: [
+          {
+            id: 23464,
+            name: "Nauloja",
+            barcode: 3377625525,
+            slot: 2,
+            level: 3,
+          },
+          {
+            id: 34523,
+            name: "Tietokone",
+            barcode: 3377625525,
+            slot: 2,
+            level: 3,
+          },
+          {
+            id: 18765,
+            name: "Näyttö",
+            barcode: 3377625525,
+            slot: 2,
+            level: 3,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    slots: [
+      {
+        barcode: 9723898802,
+        slot: 1,
+        level: 0,
+        products: [
+          {
+            id: 72341,
+            name: "Kirjoja",
+            barcode: 9723898802,
+            slot: 1,
+            level: 0,
+          },
+          {
+            id: 37654,
+            name: "Taulu",
+            barcode: 9723898802,
+            slot: 1,
+            level: 0,
+          },
+          {
+            id: 49823,
+            name: "Kahvinkeitin",
+            barcode: 9723898802,
+            slot: 1,
+            level: 0,
+          },
+          {
+            id: 49824,
+            name: "Näyttötyö",
+            barcode: 9723898802,
+            slot: 1,
+            level: 0,
+          },
+        ],
+      },
+      {
+        barcode: 9800815975,
+        slot: 1,
+        level: 1,
+        products: [
+          {
+            id: 26457,
+            name: "Kitara",
+            slot: 1,
+            level: 1,
+            barcode: 9800815975,
+          },
+          {
+            id: 72345,
+            name: "Kitaravahvistin",
+            slot: 1,
+            level: 1,
+            barcode: 9800815975,
+          },
+          {
+            id: 29864,
+            name: "Plektroja",
+            slot: 1,
+            level: 1,
+            barcode: 9800815975,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    slots: [
+      {
+        barcode: 8512751234,
+        slot: 1,
+        level: 0,
+        products: [
+          {
+            id: 98765,
+            name: "Polkupyörän runko",
+            barcode: 8512751234,
+            slot: 1,
+            level: 0,
+          },
+          {
+            id: 67654,
+            name: "Polkupyörän rengas",
+            barcode: 8512751234,
+            slot: 1,
+            level: 0,
+          },
+          {
+            id: 66543,
+            name: "Ohjaustanko",
+            barcode: 8512751234,
+            slot: 1,
+            level: 0,
+          },
+        ],
+      },
+      {
+        barcode: 8512751235,
+        slot: 1,
+        level: 1,
+        products: [
+          {
+            id: 61234,
+            name: "Polkimet",
+            barcode: 8512751235,
+            level: 1,
+            slot: 1,
+          },
+        ],
+      },
+      {
+        barcode: 8512751238,
+        slot: 1,
+        level: 2,
+        products: [
+          {
+            id: 64321,
+            name: "Jarruvaijeri",
+            barcode: 8512751238,
+            slot: 1,
+            level: 2,
+          },
+          {
+            id: 54321,
+            name: "Takavalo",
+            barcode: 8512751238,
+            slot: 1,
+            level: 2,
+          },
+          {
+            id: 99875,
+            name: "Polkupyörän ketjut",
+            barcode: 8512751238,
+            slot: 1,
+            level: 2,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 5,
+    slots: [
+      {
+        barcode: 8565487234,
+        slot: 1,
+        level: 0,
+        products: [
+          {
+            id: 22315,
+            name: "Kiekkoja",
+            barcode: 8565487234,
+            slot: 1,
+            level: 0,
+          },
+          {
+            id: 33212,
+            name: "Kaunoluistimet",
+            barcode: 8565487234,
+            slot: 1,
+            level: 0,
+          },
+          {
+            id: 11111,
+            name: "Teräsuojat",
+            barcode: 8565487234,
+            slot: 1,
+            level: 0,
+          },
+        ],
+      },
+      {
+        barcode: 8193845234,
+        slot: 1,
+        level: 1,
+        products: [
+          {
+            id: 21222,
+            name: "Kypärä",
+            barcode: 8193845234,
+            slot: 1,
+            level: 1,
+          },
+        ],
+      },
+      {
+        barcode: 1253541238,
+        slot: 2,
+        level: 0,
+        products: [
+          {
+            id: 31312,
+            name: "Kiekkoja",
+            barcode: 1253541238,
+            slot: 2,
+            level: 0,
+          },
+          {
+            id: 19043,
+            name: "Maila (vanha kunnon koho)",
+            barcode: 1253541238,
+            slot: 2,
+            level: 0,
+          },
+          {
+            id: 91111,
+            name: "Luistimet",
+            barcode: 1253541238,
+            slot: 2,
+            level: 0,
+          },
+        ],
+      },
+    ],
+  },
+];
+export default data;
